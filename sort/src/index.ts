74c12ed6a384753400e352836d2aadf64f8681de
Ben Ferriman <ben.ferriman@rtslabs.com>
2022-10-19 15:29:59 -0600

    refactored app to use abstract class rather than interface

--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,13 +1,19 @@
 import { NumbersCollection } from "./NumbersCollection";
 import { CharactersCollection } from "./CharactersCollection";
-import { Sorter } from "./Sorter";
+import { LinkedList } from "./LinkedList";
 
 const numCollection = new NumbersCollection([10, 3, -5, 0]);
-const numSorter = new Sorter(numCollection);
-numSorter.sort();
+numCollection.sort();
 console.log("Sorted Number Array: ", numCollection.data);
 
 const charCollection = new CharactersCollection("XaAjEcksoeIi");
-const charSorter = new Sorter(charCollection);
-charSorter.sort();
+charCollection.sort();
 console.log("Sorted String: ", charCollection.data);
+
+const linkedList = new LinkedList();
+linkedList.add(25);
+linkedList.add(-12);
+linkedList.add(100);
+linkedList.add(-3);
+linkedList.sort();
+linkedList.print();
